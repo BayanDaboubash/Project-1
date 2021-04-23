2b207762b19678ec35f02e76ae7abb7fa66ac203
BayanDaboubash <bayan.daboubash2000@gmail.com>
2021-04-23 18:06:10 +0300

    create page 2

--- homeJs.js
+++ homeJs.js
@@ -1,7 +1,14 @@
 const playBtn = document.querySelector(".buttonPlay");
 const popUp = document.getElementById("popup");
 const close = document.querySelector(".close");
-const name = document.getElementById("#name");
+const name = document.querySelector("#name");
+const go = document.querySelector("#go");
+const easy = document.querySelector("#easy1");
+const medium = document.querySelector("#medium1");
+const hard = document.querySelector("#hard1");
+const home = document.querySelector(".home");
+let player = "";
+let diff = "";
 
 playBtn.addEventListener("click", () => {
   popUp.style.display = "block";
@@ -9,4 +16,19 @@ playBtn.addEventListener("click", () => {
 
 close.addEventListener("click", () => {
   popUp.style.display = "none";
+});
+
+go.addEventListener("click", () => {
+  home.style.display = "none";
+});
+
+go.addEventListener("click", () => {
+  player = name.value;
+  if (easy.checked) {
+    diff = easy.value;
+  } else if (medium.checked) {
+    diff = medium.value;
+  } else if (hard.checked) {
+    diff = hard.value;
+  }
 });
